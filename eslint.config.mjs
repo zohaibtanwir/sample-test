ca345c4eca119d1d688bd67bac7e850c08a8ee49
zohaibtanwir <zohaib.tanwir@gmail.com>
2025-12-30 13:52:42 +0530

    Add eslint.config.mjs

---
+++ eslint.config.mjs
@@ -0,0 +1,26 @@
+import tseslint from '@typescript-eslint/eslint-plugin';
+import tsparser from '@typescript-eslint/parser';
+
+export default [
+  {
+    files: ['**/*.ts'],
+    languageOptions: {
+      parser: tsparser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tseslint,
+    },
+    rules: {
+      '@typescript-eslint/no-unused-vars': 'error',
+      '@typescript-eslint/explicit-function-return-type': 'warn',
+      '@typescript-eslint/no-explicit-any': 'warn',
+    },
+  },
+  {
+    ignores: ['node_modules/', 'dist/', 'playwright-report/', 'test-results/'],
+  },
+];
