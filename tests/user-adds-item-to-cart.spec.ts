4a751711d60e465adbe06888e39c9ed7e82bb3cd
zohaibtanwir <zohaib.tanwir@gmail.com>
2026-01-03 11:25:27 +0530

    Add tests/user-adds-item-to-cart.spec.ts

---
+++ tests/user-adds-item-to-cart.spec.ts
@@ -0,0 +1,13 @@
+import { test, expect } from '../src/fixtures/auth.fixture';
+
+test.describe('Product page', () => {
+  test.beforeEach(async ({ productPage }) => {
+    await productPage.goto();
+  });
+
+  test('should add item to cart', async ({ productPage }) => {
+    await productPage.clickAddToCart();
+    await expect(productPage.cartConfirmationMessage).toBeVisible();
+    await expect(productPage.cartItemCount).toContainText('1');
+  });
+});
